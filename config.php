678ef8db9027dad28c0fbb2392aa77e9786d7be9
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2024-08-02 23:27:59 -0300

    Crud_PHP

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+       // define , define uma constatante.
+
+       define('HOST', 'localhost');
+       define('USER', 'root'); 
+       define('PASS' ,'');
+       define('BASE', 'carro');
+
+       $conn = new MySQLi (HOST, USER, PASS, BASE);
+     
+
+
+
+?>
